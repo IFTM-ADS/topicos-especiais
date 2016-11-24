bea44062c7f8d8eae41a81134984ebe2c04948d2
JacqueMorais <jacquelinejmorais@gmail.com>
2016-11-24 10:46:58 -0200

    ProdutoTest

---
+++ TrabalhoTEDS/src/test/java/modelo/VendaTest.java
@@ -0,0 +1,87 @@
+package modelo;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class VendaTest {
+
+    public VendaTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+/*
+    @Test
+    public void testGetCarrinho() {
+        System.out.println("getCarrinho");
+        Venda instance = new Venda();
+        CarrinhoCompras expResult = null;
+        CarrinhoCompras result = instance.getCarrinho();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testSetCarrinho() {
+        System.out.println("setCarrinho");
+        CarrinhoCompras carrinho = null;
+        Venda instance = new Venda();
+        instance.setCarrinho(carrinho);
+    }
+
+    @Test
+    public void testGetPedido() {
+        System.out.println("getPedido");
+        Venda instance = new Venda();
+        Pedido expResult = null;
+        Pedido result = instance.getPedido();
+        assertEquals(expResult, result);
+
+    }
+
+    @Test
+    public void testSetPedido() {
+        System.out.println("setPedido");
+        Pedido pedido = null;
+        Venda instance = new Venda();
+        instance.setPedido(pedido);
+    }
+
+    @Test
+    public void testAddItem() {
+        System.out.println("addItem");
+        int quantidade = -1;
+        Produto produto = null;
+        Venda venda = new Venda();
+        CarrinhoCompras expResult = null;
+        CarrinhoCompras result = venda.addItem(quantidade, produto);
+        assertEquals(null, result);
+    }
+
+    @Test
+    public void testFinalizar() {
+        System.out.println("Finalizar");
+        CarrinhoCompras carrinho = null;
+        Venda instance = new Venda();
+        Pedido expResult = null;
+        Pedido result = instance.Finalizar(carrinho);
+        assertEquals(expResult, result);
+    }*/
+
+}
