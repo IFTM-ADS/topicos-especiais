0e7ff8f9d525fef4373fcfa3386d38f617a6898e
JacqueMorais <jacquelinejmorais@gmail.com>
2016-12-10 13:33:23 -0200

    Scripts para Inserção no Banco

---
+++ TrabalhoTEDS/ScriptInserçãoBebida.sql
@@ -0,0 +1,6 @@
+INSERT INTO `db_restaurante`.`bebida` (`codigo`, `nome`, `preco`, `descricao`) 
+VALUES 
+('2001', 'Skol', '3.5', 'desce redondo'),
+('2002', 'Cerveja', '5.5', 'cerveja puro malte'),
+('2003', 'Dolly', '1', 'Dollynho seu amiguinho'),
+('2004', 'Vinho', '89', 'vinho tinto');
---
+++ TrabalhoTEDS/ScriptInserçãoPrato.sql
@@ -0,0 +1,6 @@
+INSERT INTO `db_restaurante`.`prato` (`codigo`, `nome`, `preco`, `descricao`) 
+VALUES
+('1001', 'Salmao grelhado', '20', 'Salmao grelhado com abacaxi e lentilha'),
+('1002', 'File de pescada', '38', 'File de pescada amarela ao forno com arroz de palmito e batatas rusticas'),
+('1003', 'Talharim a carbonara', '21', 'Talharim a carbonara com aspargo e cebola caramelizada'),
+('1004', 'Costelinha', '25.5', 'Costelinha com molho barbecue');
